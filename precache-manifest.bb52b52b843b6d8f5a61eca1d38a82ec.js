960c727fe190306b936edf63ea21f1e89c979e6a
Domingo Cabeza <domingoacd1@gmail.com>
2020-07-14 16:39:43 -0400

    Updates

---
+++ precache-manifest.bb52b52b843b6d8f5a61eca1d38a82ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fcf55cfd732593262a5169d16e01d56",
+    "url": "/domingoacd.github.io/react-shoppingcart/index.html"
+  },
+  {
+    "revision": "b0d37a609b99cf4c18e5",
+    "url": "/domingoacd.github.io/react-shoppingcart/static/css/main.a92f8d64.chunk.css"
+  },
+  {
+    "revision": "019e02ae1005de8e70cf",
+    "url": "/domingoacd.github.io/react-shoppingcart/static/js/2.f9d28821.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/domingoacd.github.io/react-shoppingcart/static/js/2.f9d28821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0d37a609b99cf4c18e5",
+    "url": "/domingoacd.github.io/react-shoppingcart/static/js/main.3453db20.chunk.js"
+  },
+  {
+    "revision": "820c252b38334be81fc0",
+    "url": "/domingoacd.github.io/react-shoppingcart/static/js/runtime-main.a2a43d6b.js"
+  },
+  {
+    "revision": "d39fe1ab9e2091f16a351c0a7c57d1cb",
+    "url": "/domingoacd.github.io/react-shoppingcart/static/media/chihiro.d39fe1ab.jpg"
+  },
+  {
+    "revision": "d63cb8f73574875025e225a845feb5da",
+    "url": "/domingoacd.github.io/react-shoppingcart/static/media/totoro.d63cb8f7.jpg"
+  }
+]);
